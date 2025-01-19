650576b6654c4466f6e8304ce88325595d2d264c
yonahcitron <citronyonah@gmail.com>
2025-01-19 17:22:13 +0000

    Added more detail to my expertise, and more logo.

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -53,15 +53,14 @@ function Expertise() {
                 <div className="skill">
                     <div className="skill-icons">
                         <FontAwesomeIcon icon={faPython} size="3x"/>
-                        <img // FortAwesome doesn't have a C# icon
+                        <img // FortAwesome doesn't have a certain icons. Using raw svgs instead.
                             src={`${process.env.PUBLIC_URL}/logos/c_sharp_logo.svg`}
                             alt="C# Logo"
-                            className="icon"
                         />
                         <FontAwesomeIcon icon={faJs} size="3x"/>
                     </div>
-                    <h3>Full Stack for Big Data</h3>
-                    <p>I have designed and implemented large-scale data pipelines, creating robust backends for data processing and frontends for data visualization and interaction. I excel at delivering end-to-end solutions, combining expertise in the software development life cycle (SDLC) as well as the modern data stack, including tools and technologies for data ingestion, transformation, and orchestration.</p>
+                    <h3>Scalable Data Applications</h3>
+                    <p>I leverage full-stack expertise to create robust backends for data processing, and scalable frontends for data visualization and interaction. I enable powerful decision-making with large-scale data pipelines, utilizing modern technologies for data ingestion, transformation, and orchestration to drive actionable insights on structured and unstructured data, and deliver business value.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsFirst.map((label, index) => (
@@ -71,9 +70,19 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
+                    <div className="skill-icons">
+                        <FontAwesomeIcon icon={faDocker} size="3x"/>
+                        <img
+                            src={`${process.env.PUBLIC_URL}/logos/azure_logo.svg`}
+                            alt="Azure Logo"
+                        />
+                        <img
+                            src={`${process.env.PUBLIC_URL}/logos/bash_logo.svg`}
+                            alt="Bash Logo"
+                        />
+                    </div>
                     <h3>DevOps & Infra Automation</h3>
-                    <p>I incorporate cloud-native DevOps practices throughout the entire development lifecycle. I leverage infrastructure-as-code (IaC), CI/CD pipelines, and robust testing to ensure quick deployment from development to production.</p>
+                    <p>I incorporate cloud-native DevOps practices throughout the entire development lifecycle. I leverage infrastructure-as-code (IaC), CI/CD pipelines, and robust testing to ensure quick deployment from development to production. I leverage my expertise across the the software development life cycle to deliver comprehensive and reliable end-to-end solutions.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsSecond.map((label, index) => (
@@ -83,7 +92,20 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
+                    <div className="skill-icons">
+                        <img
+                            src={`${process.env.PUBLIC_URL}/logos/hf_logo.svg`}
+                            alt="HuggingFace Logo"
+                        />
+                        <img
+                            src={`${process.env.PUBLIC_URL}/logos/azureml_logo.svg`}
+                            alt="AzureML Logo"
+                        />
+                        <img
+                            src={`${process.env.PUBLIC_URL}/logos/openai_logo.svg`}
+                            alt="OpenAI Logo"
+                        />
+                    </div>
                     <h3>GenAI & LLMs</h3>
                     <p>I integrate secure Large Language Models (LLMs) into applications to help clients unlock the power of AI for smarter decision-making. With experience building scalable GenAI solutions, I focus on automating workflows, enhancing data pipelines, and delivering intelligent features like insight generation and semantic search, all while ensuring secure and efficient deployment.</p>
                     <div className="flex-chips">
